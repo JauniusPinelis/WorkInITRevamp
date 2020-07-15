89ee030af9ee75aa99bf644af6976bdb11409f43
Jaunius Pinelis <JauniusPinelis@gmail.com>
2020-07-15 23:02:03 +0300

    Initial project commit

---
+++ Data/JobScraper/Domain/Models/JobUrl.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Domain.Models
+{
+	public class JobUrl
+	{
+		public string Url { get; set; }
+		public string Title { get; set; }
+		public string Description { get; set; }
+		public string Salary { get; set; }
+	}
+}
---
+++ Data/JobScraper/Infrastructure/DataContext.cs
@@ -0,0 +1,13 @@
+﻿using Domain.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Infrastructure
+{
+	public class DataContext : DbContext
+	{
+		public DbSet<JobUrl> JobUrls { get; set; }
+	}
+}
