ba961fd34cce6a9bd3baa77a7a3eccd2f5f17623
Jaunius Pinelis <JauniusPinelis@gmail.com>
2020-09-17 08:09:33 +0300

    test fixes

--- Data/JobScraper/Application/Configuration/CvBankasConfiguration.cs
+++ Data/JobScraper/Application/Configuration/CvBankasConfiguration.cs
@@ -10,8 +10,8 @@ namespace Application.Configuration
 	{
 		public string Posting { get; set; } = "article";
 		public string Name { get; set; } = "h3.list_h3";
-		public string Salary { get; set; } = "span.dib.mt5";
-		public string Company { get; set; } = "salary_amount";
+		public string Salary { get; set; } = "span.salary_amount";
+		public string Company { get; set; } = "span.dib.mt5";
 		public string Url { get; set; } = "a.list_a";
  	}
 }
