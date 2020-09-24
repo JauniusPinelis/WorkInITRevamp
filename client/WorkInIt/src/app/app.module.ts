d3cdc0379af748bff9e498b32f2a6996401a4989
Jaunius Pinelis <JauniusPinelis@gmail.com>
2020-09-24 15:58:26 +0300

    Added Ant design

--- client/WorkInIt/src/app/app.module.ts
+++ client/WorkInIt/src/app/app.module.ts
@@ -7,6 +7,13 @@ import { JobtableComponent } from './components/jobtable/jobtable.component';
 
 import { FormsModule } from '@angular/forms';
 import { HttpClientModule } from '@angular/common/http';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { NZ_I18N } from 'ng-zorro-antd/i18n';
+import { en_US } from 'ng-zorro-antd/i18n';
+import { registerLocaleData } from '@angular/common';
+import en from '@angular/common/locales/en';
+
+registerLocaleData(en);
 
 @NgModule({
   declarations: [
@@ -17,9 +24,10 @@ import { HttpClientModule } from '@angular/common/http';
     BrowserModule,
     AppRoutingModule,
     FormsModule,
-    HttpClientModule
+    HttpClientModule,
+    BrowserAnimationsModule
   ],
-  providers: [],
+  providers: [{ provide: NZ_I18N, useValue: en_US }],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
