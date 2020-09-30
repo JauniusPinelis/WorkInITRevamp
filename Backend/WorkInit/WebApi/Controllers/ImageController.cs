fb1a49b3628f395d11673889cee6e7a4bfbea948
Jaunius Pinelis <JauniusPinelis@gmail.com>
2020-09-30 20:59:55 +0300

    Started coding for ImageController

---
+++ Backend/WorkInit/WebApi/Controllers/ImageController.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WebApi.Controllers
+{
+    public class ImageController
+    {
+        //https://stackoverflow.com/questions/62847617/get-imagebinary-data-from-database-with-net-core-and-angular-8
+        [HttpGet]
+        [FileResultContentType("image/jpeg")]
+        public async Task<FileStreamResult> GetImageStream(string imageId)
+        {
+            Stream imageStream = await _imageStore.GetImage(imageId);
+            imageStream.Seek(0, SeekOrigin.Begin);
+
+            var contentDisposition = new ContentDispositionHeaderValue("attachment");
+            contentDisposition.SetHttpFileName($"{imageId}.jpg");
+            Response.Headers.Add(HeaderNames.ContentDisposition, contentDisposition.ToString());
+
+            return new FileStreamResult(imageStream, "image/jpeg");
+        }
+    }
+}
