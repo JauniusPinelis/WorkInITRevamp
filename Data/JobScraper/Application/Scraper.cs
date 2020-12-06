a70ab5c08fafaf9ba0aa8b22b9b6721a6703cdaa
Jaunius Pinelis <JauniusPinelis@gmail.com>
2020-12-07 00:24:29 +0200

    Added scrutor

--- Data/JobScraper/Application/Scraper.cs
+++ Data/JobScraper/Application/Scraper.cs
@@ -2,19 +2,14 @@
 using HtmlAgilityPack;
 using ScrapySharp.Network;
 using System;
-using System.Collections.Generic;
 using System.IO;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.Drawing;
-using System.Net.Http;
 using System.Net;
+using System.Text;
 
 namespace Application
 {
-    public class Scraper : IScraper
-    {
+	public class Scraper : IScraper
+	{
 		private readonly ScrapingBrowser _browser;
 
 		public Scraper()
@@ -30,95 +25,95 @@ namespace Application
 			return homePage.Html;
 		}
 
-        public byte[] GetImage(string url)
+		public byte[] GetImage(string url)
 		{
-            Stream stream = null;
-            byte[] buf;
+			Stream stream = null;
+			byte[] buf;
 
-            try
-            {
-                url = url.Replace("https", "http");
-
-                if (!url.StartsWith("http"))
-                {
-                    return null;
-                }
-
-                WebProxy myProxy = new WebProxy();
-                HttpWebRequest req = (HttpWebRequest)WebRequest.Create(url);
-
-                HttpWebResponse response = (HttpWebResponse)req.GetResponse();
-                stream = response.GetResponseStream();
-
-                using (BinaryReader br = new BinaryReader(stream))
-                {
-                    int len = (int)(response.ContentLength);
-                    buf = br.ReadBytes(len);
-                    br.Close();
-                }
-
-                stream.Close();
-                response.Close();
-            }
-            catch (Exception exp)
-            {
-                buf = null;
-            }
-
-            return (buf);
-        }
-
-        public byte[] GetImage2(string url)
-        {
-            url = url.Replace("https", "http");
-            
-            if (!url.StartsWith("http"))
+			try
 			{
-                return null;
+				url = url.Replace("https", "http");
+
+				if (!url.StartsWith("http"))
+				{
+					return null;
+				}
+
+				WebProxy myProxy = new WebProxy();
+				HttpWebRequest req = (HttpWebRequest)WebRequest.Create(url);
+
+				HttpWebResponse response = (HttpWebResponse)req.GetResponse();
+				stream = response.GetResponseStream();
+
+				using (BinaryReader br = new BinaryReader(stream))
+				{
+					int len = (int)(response.ContentLength);
+					buf = br.ReadBytes(len);
+					br.Close();
+				}
+
+				stream.Close();
+				response.Close();
+			}
+			catch (Exception exp)
+			{
+				buf = null;
 			}
 
-            HttpWebRequest request = (HttpWebRequest)WebRequest.Create(url);
-            HttpWebResponse response;
-            try
-            {
-                response = (HttpWebResponse)request.GetResponse();
-            }
-            catch (Exception ex )
-            {
-                return null;
-            }
+			return (buf);
+		}
 
-            // Check that the remote file was found. The ContentType
-            // check is performed since a request for a non-existent
-            // image file might be redirected to a 404-page, which would
-            // yield the StatusCode "OK", even though the image was not
-            // found.
-            if ((response.StatusCode == HttpStatusCode.OK ||
-                response.StatusCode == HttpStatusCode.Moved ||
-                response.StatusCode == HttpStatusCode.Redirect) &&
-                response.ContentType.StartsWith("image", StringComparison.OrdinalIgnoreCase))
-            {
+		public byte[] GetImage2(string url)
+		{
+			url = url.Replace("https", "http");
 
-                // if the remote file was found, download it
-                using (Stream inputStream = response.GetResponseStream()) 
-                {
-                
-                    byte[] buffer = new byte[4096];
-                    int bytesRead;
-                    do
-                    {
-                        bytesRead = inputStream.Read(buffer, 0, buffer.Length);
-                        
-                    } while (bytesRead != 0);
+			if (!url.StartsWith("http"))
+			{
+				return null;
+			}
 
-                    return buffer;
-                }
+			HttpWebRequest request = (HttpWebRequest)WebRequest.Create(url);
+			HttpWebResponse response;
+			try
+			{
+				response = (HttpWebResponse)request.GetResponse();
+			}
+			catch (Exception ex)
+			{
+				return null;
+			}
 
-            }
-            else
-                return null;
-        }
+			// Check that the remote file was found. The ContentType
+			// check is performed since a request for a non-existent
+			// image file might be redirected to a 404-page, which would
+			// yield the StatusCode "OK", even though the image was not
+			// found.
+			if ((response.StatusCode == HttpStatusCode.OK ||
+				response.StatusCode == HttpStatusCode.Moved ||
+				response.StatusCode == HttpStatusCode.Redirect) &&
+				response.ContentType.StartsWith("image", StringComparison.OrdinalIgnoreCase))
+			{
+
+				// if the remote file was found, download it
+				using (Stream inputStream = response.GetResponseStream())
+				{
+
+					byte[] buffer = new byte[4096];
+					int bytesRead;
+					do
+					{
+						bytesRead = inputStream.Read(buffer, 0, buffer.Length);
+
+					} while (bytesRead != 0);
+
+					return buffer;
+				}
+
+			}
+			else
+				return null;
+		}
 
 
-    }
+	}
 }
--- Data/JobScraper/Runner/Program.cs
+++ Data/JobScraper/Runner/Program.cs
@@ -1,6 +1,5 @@
 ﻿using Application;
 using Application.DataServices;
-using Application.Interfaces;
 using Application.Services;
 using AutoMapper;
 using Domain.Configuration;
@@ -9,81 +8,83 @@ using Infrastructure.Repositories;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
-using System;
-using System.Configuration;
 using System.IO;
 
 namespace Runner
 {
 	public class Program
-    {
-        static void Main(string[] args)
-        {
-            var services = ConfigureServices();
+	{
+		static void Main(string[] args)
+		{
+			var services = ConfigureServices();
 
-            var serviceProvider = services.BuildServiceProvider();
+			var serviceProvider = services.BuildServiceProvider();
 
-            using (var client = new DataContext())
-            {
-                client.Database.Migrate();
-            }
+			using (var client = new DataContext())
+			{
+				client.Database.Migrate();
+			}
 
-            // calls the Run method in App, which is replacing Main
-            serviceProvider.GetService<JobScraperRunner>().Run();
-        }
+			// calls the Run method in App, which is replacing Main
+			serviceProvider.GetService<JobScraperRunner>().Run();
+		}
 
-        private static IServiceCollection ConfigureServices()
-        {
+		private static IServiceCollection ConfigureServices()
+		{
 
-            IServiceCollection services = new ServiceCollection();
+			IServiceCollection services = new ServiceCollection();
 
-            var config = LoadConfiguration();
-            services.AddSingleton(config);
+			var config = LoadConfiguration();
+			services.AddSingleton(config);
 
-            // required to run the application
+			// required to run the application
+			services.Scan(scan =>
+		scan.FromCallingAssembly()
+			.AddClasses()
+			.AsMatchingInterface());
 
-            services.AddScoped<JobScraperRunner>();
-            services.AddScoped<DataService>();
-            services.AddScoped<CvOnlineScrapeService>();
-            services.AddScoped<CvBankasScrapeService>();
-            services.AddScoped<CvMarketScrapeService>();
-            services.AddScoped<UnitOfWork>();
-            services.AddScoped<IScraper, Scraper>();
-            services.AddScoped<CvBankasConfiguration>();
-            services.AddScoped<CvOnlineConfiguration>();
-            services.AddScoped<CvMarketConfiguration>();
-            services.AddScoped<CvOnlineDataService>();
-            services.AddScoped<CvBankasDataService>();
-            services.AddScoped<CvMarketDataService>();
-            services.AddScoped<CvOnlineRepostory>();
-            services.AddScoped<CvBankasRepository>();
-            services.AddScoped<CvMarketRepository>();
+			services.AddScoped<JobScraperRunner>();
+			services.AddScoped<DataService>();
+			services.AddScoped<CvOnlineScrapeService>();
+			services.AddScoped<CvBankasScrapeService>();
+			services.AddScoped<CvMarketScrapeService>();
+			services.AddScoped<UnitOfWork>();
+			//services.AddScoped<IScraper, Scraper>();
+			services.AddScoped<CvBankasConfiguration>();
+			services.AddScoped<CvOnlineConfiguration>();
+			services.AddScoped<CvMarketConfiguration>();
+			services.AddScoped<CvOnlineDataService>();
+			services.AddScoped<CvBankasDataService>();
+			services.AddScoped<CvMarketDataService>();
+			services.AddScoped<CvOnlineRepostory>();
+			services.AddScoped<CvBankasRepository>();
+			services.AddScoped<CvMarketRepository>();
 
-            services.AddScoped<CompanyService>();
+			services.AddScoped<CompanyService>();
 
-            services.AddDbContext<DataContext>(options
-                => options.UseSqlServer(config.GetConnectionString("DefaultConnection")));
+			services.AddDbContext<DataContext>(options
+				=> options.UseSqlServer(config.GetConnectionString("DefaultConnection")));
 
-            //Automapper
-            var mapperConfig = new MapperConfiguration(mc =>
-            {
-                mc.AddProfile(new MappingProfile());
-            });
+			//Automapper
+			var mapperConfig = new MapperConfiguration(mc =>
+			{
+				mc.AddProfile(new MappingProfile());
+			});
 
-            IMapper mapper = mapperConfig.CreateMapper();
-            services.AddSingleton(mapper);
+			IMapper mapper = mapperConfig.CreateMapper();
+			services.AddSingleton(mapper);
 
 
-            return services;
-        }
+			return services;
+		}
 
-        public static IConfiguration LoadConfiguration()
-        {
-            var builder = new ConfigurationBuilder()
-                .SetBasePath(Directory.GetCurrentDirectory())
-                .AddJsonFile("appsettings.json", optional: false);
+		public static IConfiguration LoadConfiguration()
+		{
+			var builder = new ConfigurationBuilder()
+				.SetBasePath(Directory.GetCurrentDirectory())
+				.AddJsonFile("appsettings.json", optional: false);
 
-            return builder.Build();
-        }
-    }
+			return builder.Build();
+		}
+	}
 }
